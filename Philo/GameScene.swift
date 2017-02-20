ce83c6a7e2fa84a1b5ac3776e801d4927cd6d294
Ge0rges <ge0rges@ge0rges.com>
2017-02-20 14:09:02 +0200

    Only Tap Black. Final.

--- Philo/GameScene.swift
+++ Philo/GameScene.swift
@@ -10,101 +10,154 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
+  
+  var entities = [GKEntity]()
+  var graphs = [String : GKGraph]()
+  
+  private var lastUpdateTime : TimeInterval = 0// Used to track the time between frames
+  private var label : SKLabelNode!// Main label that shows how to play and game over.
+  
+  private var touchValid : Bool = true// Used to track wether a color is on screen (hence the touch is valid and won't cause a game over)
+  private var randomSource : GKLinearCongruentialRandomSource!// Random numbe rgenerator
+  private var gamePlaying : Bool = false// Used to track the state of the game
 
-        self.lastUpdateTime = 0
+  private var secondsTillPressValid : Float = 0// Stores the seconds till touchValid should bne true, and since it was if < 0
+  private var timeForBlack : Float = 0// Holds the randomly generated value time for black
+  private let maxBlackDisplayTime : Float = 5.0// Max display time for black, in seconds.
+  private let maxTimeForColors : Float = 20.0// Max time the colors should be shown, in seconds.
+  private let maxTimePerColor : Float = 7.0// Max time an individual color should be shown, in seconds.
+  
+  private var score : Int = 0;
+  
+  override func sceneDidLoad() {
+    
+    self.lastUpdateTime = 0// Initialize
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+    // Get label node from scene and store it for use later
+    self.label = self.childNode(withName: "helloLabel") as? SKLabelNode
+    
+    // Initialize the random number source
+    self.randomSource = GKLinearCongruentialRandomSource.init()
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    // If game is in progress, handle the touch
+    if self.gamePlaying {
+      self.handleGameTouch();
+      return;
+    }
+    
+    // Remove the label by scaling it out, on first touch.
+    if (!self.gamePlaying) {
+      self.gamePlaying = true
+      self.score = 0
+      
+      self.label.run(SKAction.init(named: "FadeOut")!, withKey: "fadeOut")
+      self.label.text = "Hits: \(self.score)"
+      
+      // Start the game
+      self.touchValid = false
+      self.secondsTillPressValid = self.randomSource.nextUniform()*maxTimeForColors;
+      
+      // Randomize the background color
+      self.randomizeBackgroundColorUntilBlack()
+    }
+  }
+  
+  func handleGameTouch() {// Will only be called by a touch if gamePlaying is true
+    if self.touchValid && self.gamePlaying {// Check if the user was right to press
+      // Reset
+      self.touchValid = false
+      self.secondsTillPressValid = self.randomSource.nextUniform()*maxTimeForColors;
+      
+      // Randomize the background color
+      self.randomizeBackgroundColorUntilBlack()
+      
+      // Update score
+      self.score += 1
+      self.label.text = "Hits: \(self.score)"
+      
+    } else if !self.touchValid && self.gamePlaying {// Game over
+      // Return game over label to front
+      self.label.text = "You Lost: \(score)"
+      self.label.run(SKAction.init(named: "FadeIn")!, withKey: "fadeIn")
+      
+      // Reset the game
+      self.touchValid = true
+      self.secondsTillPressValid = 0
+      self.gamePlaying = false
+      self.score = 0
+    }
+  }
+  
+  override func update(_ currentTime: TimeInterval) {// Called before each frame is rendered
+    // Initialize lastUpdateTime if it has not already been
+    if (self.lastUpdateTime == 0 || self.isPaused) {
+      self.lastUpdateTime = currentTime
+    }
+    
+    // Calculate time since last update
+    let dt = currentTime - self.lastUpdateTime
+    
+    // Update entities
+    for entity in self.entities {
+      entity.update(deltaTime: dt)
+    }
+    
+    self.lastUpdateTime = currentTime
+    
+    // If the game is waiting to allow a valid press, decrement the number of frames till press valid.
+    if self.gamePlaying {
+      self.secondsTillPressValid -= Float(dt);// Substract the time in seconds since the last frame.
+      
+      
+      // Check if we should allow a valid press and color black
+      if (self.secondsTillPressValid <= 0 && !self.touchValid) {
+        // First allow touches and reset the framesTillValid
+        self.touchValid = true
+        self.secondsTillPressValid = 0
+        
+        // Black background
+        self.backgroundColor = UIColor.black
+        
+        // Time the black should be displayed. Between 0 and 5 seconds.
+        self.timeForBlack = self.randomSource.nextUniform()*maxBlackDisplayTime
+        if self.timeForBlack < 0.5 {
+          self.timeForBlack = 0.5;
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        return;
+      }
+      
+      // Check if the background is black (touch valid) and the time has run out
+      if (self.secondsTillPressValid <= -self.timeForBlack && self.touchValid) {
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        // Reset
+        self.touchValid = false
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        // Handle as if a touch to go to game over if necessary.
+        self.handleGameTouch()
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
+        return;
+      }
     }
+  }
+  
+  func randomizeBackgroundColorUntilBlack() {
+    if self.backgroundColor != UIColor.black && self.gamePlaying {
+      let timeForColor = self.randomSource.nextUniform()*maxTimePerColor// Generate a first random time
+      
+      // Color the background
+      let red = CGFloat(self.randomSource.nextUniform())
+      let green = CGFloat(self.randomSource.nextUniform())
+      let blue = CGFloat(self.randomSource.nextUniform())
+      
+      self.backgroundColor = UIColor.init(red: red, green: green, blue: blue, alpha: 1.0)
+      
+      let dispatchTime = DispatchTime.now() + Double(timeForColor)
+      DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
+        self.randomizeBackgroundColorUntilBlack()
+      }
+    }
+  }
 }
